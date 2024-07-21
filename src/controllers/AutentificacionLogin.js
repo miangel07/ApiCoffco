c66e9cfd1b44f74a7cff3b539fcf6bf88f665a5d
daniel22alejandro <danniel10yara@gmail.com>
2024-07-20 21:13:32 -0500

    crud usuarios corregido

--- src/controllers/AutentificacionLogin.js
+++ src/controllers/AutentificacionLogin.js
@@ -32,10 +32,10 @@ export const validarUsuarios = async (req, res) => {
             return res.status(400).json(error)
         }
 
-        let { id: numero_identificacion,password: contraseña_usuario } = req.body
+        let { id: numero_documento,password: password } = req.body
         
         // Consulta para obtener el hash de la contraseña del usuario desde la base de datos
-        let sql =`SELECT nombre_usuario, rol_usuario, contraseña_usuario FROM usuarios WHERE numero_identificacion='${numero_identificacion}'`
+        let sql =`SELECT numero_documento, rol_usuario, password FROM usuarios WHERE numero_documento='${numero_documento}'`
         const [resultado] = await conexion.query(sql);
       
 
@@ -44,12 +44,12 @@ export const validarUsuarios = async (req, res) => {
             const storedPasswordHash = user.contraseña_usuario;
 
             // Comparación de la contraseña proporcionada con el hash almacenado en la base de datos
-            const passwordMatch = await bcryptjs.compare(contraseña_usuario, storedPasswordHash);
+            const passwordMatch = await bcryptjs.compare(password, storedPasswordHash);
 
             if (passwordMatch) {
                 // Contraseña válida: se genera un token de autenticación
                 let token = jwt.sign({user:resultado},process.env.SECRET,{expiresIn:process.env.TIME})
-                return res.status(200).json({ user: { nombre_usuario: user.nombre_usuario, rol_usuario: user.rol_usuario }, token, message: 'Usuario autorizado' });
+                return res.status(200).json({ user: { nombre: user.nombre, rol_usuario: user.rol_usuario }, token, message: 'Usuario autorizado' });
             } else {
                 // Contraseña no válida
                 return res.status(401).json({ message: 'Contraseña incorrecta' });
