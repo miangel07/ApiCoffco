e9509ab827275a2e3a0b062836f5f2d022992e99
miangel07 <miguelosoriorojas063@gmail.com>
2024-07-21 23:40:33 -0500

    version validation

--- validation/ambienteValidation.js
+++ validation/ambienteValidation.js
@@ -1,7 +1,7 @@
 import { check } from "express-validator";
 
 export const ambienteValidate = [
-  check("nombre","El nombre es obligatorio").not().isEmpty().isInt().isLength({max:50}),
+  check("nombre","El nombre es obligatorio").not().isEmpty().isLength({max:50}),
   check("estado", "El estado es obligatorio")
     .optional()
     .isIn(['activo', 'inactivo'])
