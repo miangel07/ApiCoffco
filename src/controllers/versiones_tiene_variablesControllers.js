a23e85bf4f59d528473c8766fa8211b430330c2c
miangel07 <miguelosoriorojas063@gmail.com>
2024-07-27 17:03:35 -0500

    merge jonathan

--- src/controllers/versiones_tiene_variablesControllers.js
+++ src/controllers/versiones_tiene_variablesControllers.js
@@ -1,24 +1,24 @@
 import { validationResult } from 'express-validator';
-import {conexion} from '../database/conexion.js';
+import { conexion } from '../database/conexion.js';
 
 
-export const registrarVersionesTieneVariables=async(req,res)=>{
-    try {
-      const error = validationResult(req)
-      if(!error.isEmpty()){
-        return res.status(400).json(error)
-      }
-        let {fk_versiones,fk_variables}=req.body
-        let sql = `insert into versiones_tiene_variables (fk_versiones,fk_variables)values(?,?)`;
-        const [respuesta]=await conexion.query(sql,[fk_versiones,fk_variables])
-        if(respuesta.affectedRows==1){
-            res.status(200).json({message:'Version y variable registrada con exito'})
-        }else{
-            res.status(404).json({ message: "Version y variable no registrada" });
-        }
-    } catch (error) {
-        res.status(500).json({message:'Error en el servidor'+error.message})
+export const registrarVersionesTieneVariables = async (req, res) => {
+  try {
+    const error = validationResult(req)
+    if (!error.isEmpty()) {
+      return res.status(400).json(error)
     }
+    let { fk_versiones, fk_variables } = req.body
+    let sql = `insert into versiones_tiene_variables (fk_versiones,fk_variables)values(?,?)`;
+    const [respuesta] = await conexion.query(sql, [fk_versiones, fk_variables])
+    if (respuesta.affectedRows == 1) {
+      res.status(200).json({ message: 'Version y variable registrada con exito' })
+    } else {
+      res.status(404).json({ message: "Version y variable no registrada" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error en el servidor' + error.message })
+  }
 }
 
 export const listarVersionesTieneVariables = async (req, res) => {
@@ -51,10 +51,10 @@ export const listarVersionesTieneVariables = async (req, res) => {
   }
 };
 
-export const listarVersionesTieneVariablesId = async (req,res)=>{
-    try {
-        let id = req.params.id
-        let sql = `select 
+export const listarVersionesTieneVariablesId = async (req, res) => {
+  try {
+    let id = req.params.id
+    let sql = `select 
         vtv.id_versiones_tiene_variables, 
         v.idVersion as 'id de version', 
         v.version as 'nombre version',
@@ -67,15 +67,15 @@ export const listarVersionesTieneVariablesId = async (req,res)=>{
         versiones v on vtv.fk_versiones = v.idVersion
         join 
         variables var on vtv.fk_variables = var.idVariable where id_versiones_tiene_variables = ? `;
-        const [respuesta]=await conexion.query(sql,[id])
-        if(respuesta.length==1){
-            res.status(200).json(respuesta)
-        }else{
-            res.status(404).json({message:'Referencia de versiones y variables no encontrada'})
-        }
-    } catch (error) {
-        res.status(500).json({message:'Error en el servidor'+error.message})
+    const [respuesta] = await conexion.query(sql, [id])
+    if (respuesta.length == 1) {
+      res.status(200).json(respuesta)
+    } else {
+      res.status(404).json({ message: 'Referencia de versiones y variables no encontrada' })
     }
+  } catch (error) {
+    res.status(500).json({ message: 'Error en el servidor' + error.message })
+  }
 }
 
 export const actualizarVersionesTieneVariables = async (req, res) => {
@@ -84,11 +84,11 @@ export const actualizarVersionesTieneVariables = async (req, res) => {
     if (!error.isEmpty()) {
       return res.status(400).json(error);
     }
-    
+
     let { fk_versiones, fk_variables } = req.body;
-    let id= req.params.id
+    let id = req.params.id
     let sql = `update versiones_tiene_variables set fk_versiones=?,fk_variables=? where id_versiones_tiene_variables=?`;
-    const [respuesta] = await conexion.query(sql, [fk_versiones, fk_variables,id]);
+    const [respuesta] = await conexion.query(sql, [fk_versiones, fk_variables, id]);
     if (respuesta.affectedRows == 1) {
       res
         .status(200)
