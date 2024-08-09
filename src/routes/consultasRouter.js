ed1c9a3486c324c1d2e0bce0534261eabc304305
Lumaryy <luisamariasancheztovar2006@gmail.com>
2024-08-08 21:25:45 -0500

    validates faltantes

--- src/routes/consultasRouter.js
+++ src/routes/consultasRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { consultaRegistroIngresoTostion, consultaSalidaserviciosTostionyTrilla } from "../controllers/consultasController.js";
+import { validateConsultas } from "../../validation/consultasValidate.js";
+import { validarToken } from "../controllers/AutentificacionLogin.js";
 
 const routerConsultas = express.Router();
 
-routerConsultas.post('/reporteingreso',consultaRegistroIngresoTostion)
-routerConsultas.post('/reportesalida',consultaSalidaserviciosTostionyTrilla)
+routerConsultas.post('/reporteingreso',validarToken,validateConsultas,consultaRegistroIngresoTostion)
+routerConsultas.post('/reportesalida',validarToken,validateConsultas,consultaSalidaserviciosTostionyTrilla)
 
 
 export default routerConsultas;
--- validation/serviciosValidation.js
+++ validation/serviciosValidation.js
@@ -1,8 +1,32 @@
 import { check } from "express-validator";
 
 export const validateServicios = [
-    check('nombre', 'El nombre es obligatorio y debe ser una cadena de texto válida')
-        .not().isEmpty().withMessage('El nombre no debe estar vacío')
+    check('nombre')
+        .not().isEmpty().withMessage('El nombre es obligatorio y no debe estar vacío')
         .isString().withMessage('El nombre debe ser una cadena de texto')
         .isLength({ max: 100 }).withMessage('El nombre no debe exceder 100 caracteres'),
+    
+    check('fk_idTipoServicio')
+        .not().isEmpty().withMessage('El ID de tipo de servicio es obligatorio')
+        .isInt().withMessage('El ID de tipo de servicio debe ser un número entero'),
+    
+    check('fecha')
+        .not().isEmpty().withMessage('La fecha es obligatoria')
+        .isISO8601().withMessage('La fecha debe estar en un formato válido (ISO 8601)'),
+    
+    check('fk_idAmbiente')
+        .not().isEmpty().withMessage('El ID del ambiente es obligatorio')
+        .isInt().withMessage('El ID del ambiente debe ser un número entero'),
+    
+    check('fk_idMuestra')
+        .not().isEmpty().withMessage('El ID de muestra es obligatorio')
+        .isInt().withMessage('El ID de muestra debe ser un número entero'),
+    
+    check('fk_idPrecio')
+        .not().isEmpty().withMessage('El ID del precio es obligatorio')
+        .isInt().withMessage('El ID del precio debe ser un número entero'),
+    
+    check('fk_idUsuarios')
+        .not().isEmpty().withMessage('El ID del usuario es obligatorio')
+        .isInt().withMessage('El ID del usuario debe ser un número entero'),
 ];
