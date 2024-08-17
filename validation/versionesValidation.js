2d367cd5e226c72ef5579155603fdea6a352498d
SS-victoria <stiven-cool@hotmail.com>
2024-08-16 22:17:11 -0500

    actualicion de controladores de variables, versiones, valor y documentos por la modificacion 1.8 a la base de datos

--- validation/versionesValidation.js
+++ validation/versionesValidation.js
@@ -1,13 +1,10 @@
 import { check } from "express-validator";
 export const validateVersiones = [
+
   check('version', 'La versión es obligatoria y debe ser una cadena de texto válida')
   .not().isEmpty().withMessage('La versión no debe estar vacía')
   .isString().withMessage('La versión debe ser una cadena de texto')
-  .isLength({ max: 50 }).withMessage('La versión no debe exceder 50 caracteres'),
-
-check('fk_id_usuarios', 'El ID del usuario es obligatorio y debe ser un número entero positivo')
-  .not().isEmpty().withMessage('El ID del usuario no debe estar vacío')
-  .isInt({ gt: 0 }).withMessage('El ID del usuario debe ser un número entero positivo'),
+  .isLength({ max: 100 }).withMessage('La versión no debe exceder 50 caracteres'),
 
 check('fk_documentos', 'El ID del documento es obligatorio y debe ser un número entero positivo')
   .not().isEmpty().withMessage('El ID del documento no debe estar vacío')
