1d5ffd0b003a7400a7597141f9d9b7ec31f1d937
daniel22alejandro <danniel10yara@gmail.com>
2024-09-03 16:32:49 -0500

    empezando el crud de muestras

--- src/routes/muestraRoutes.js
+++ src/routes/muestraRoutes.js
@@ -4,11 +4,11 @@ import { validarToken } from "../controllers/AutentificacionLogin.js";
 import { validateMuestra } from "../../validation/muestraValidation.js";
 const ruta=Router()
 
-ruta.get("/listar",validarToken,ListarMuestras)
-ruta.post("/registrar",validarToken,validateMuestra,RegistrarMuestra)
-ruta.put("/actualizar/:id",validarToken,validateMuestra,ActualizarMuestra)
-ruta.delete("/eliminar/:id",validarToken,EliminarMuestra)
-ruta.get("/listarid/:id",validarToken,ListaridMuestra)
+ruta.get("/listar",ListarMuestras)
+ruta.post("/registrar",validateMuestra,RegistrarMuestra)
+ruta.put("/actualizar/:id",validateMuestra,ActualizarMuestra)
+ruta.delete("/eliminar/:id",EliminarMuestra)
+ruta.get("/listarid/:id",ListaridMuestra)
 
 export default ruta
 
