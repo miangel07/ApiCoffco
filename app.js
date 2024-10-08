7e2eb2a9e7be0ae7877456cdb1481d6dfa2e1b92
miangel07 <miguelosoriorojas063@gmail.com>
2024-10-07 22:03:17 -0500

    configuracion de swagger para la documentacion

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-import express from "express"; 
+import express from "express";
 import bodyParser from "body-parser";
 import path from 'path';
-import cors from "cors"; 
+import cors from "cors";
 import { fileURLToPath } from 'url';
 import router from "./src/routes/index.js";
-
+import { swaggerUi, swaggerSetup } from "./views/swagger.js";
 //OBTENER EL NOMBRE DE LA CARPETA 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const servidor = express(); 
+const servidor = express();
 servidor.use(cors({
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'token'], 
-    credentials: true, 
+    allowedHeaders: ['Content-Type', 'Authorization', 'token'],
+    credentials: true,
 }));
 servidor.use(bodyParser.json({ limit: '50mb' }));
 servidor.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 servidor.set('view engine', 'ejs');
 servidor.set('views', './views');
+servidor.use("/docs", swaggerUi.serve, swaggerSetup)
 //HACER PUBLICA LA CARPETA PUBLIC PARA PODER ACCEDER A ELLA EN EL FRONTEND
 servidor.use('/public', express.static(path.join(__dirname, 'public')));
 
 servidor.use('/documents', (req, res) => {
     res.render('documentacion.ejs');
 });
-servidor.use("/api",router)
+servidor.use("/api", router)
 
 servidor.listen(3000, () => {
     console.log("servidor escuchando desde el puerto  3000");
---
+++ views/swagger.js
@@ -0,0 +1,31 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { usuariosDocs } from '../src/swagger/usuarios.js';
+import { reportesDocs } from '../src/swagger/reportes.js';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'COFFCO ',
+      version: '1.0.0',
+      description: 'Documentación de Coffco',
+    },
+     servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+    paths: {
+      ...reportesDocs,
+      ...usuariosDocs,
+
+    },
+  },
+  apis: ['./src/swagger/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+const swaggerSetup = swaggerUi.setup(specs);
+
+export { swaggerUi, swaggerSetup };
