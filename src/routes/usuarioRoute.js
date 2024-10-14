2a111e2947eb31e36af5ed8c1d46e540ebdf3aa7
Luisa <luisamariasancheztovar2006@gmail.com>
2024-10-14 16:34:35 -0500

    luisa

--- src/routes/usuarioRoute.js
+++ src/routes/usuarioRoute.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { actualizarUsuario, eliminarUsuario, listarUsuario, listarUsuarioId, registrarUsuario,ConsultaUsers, estadoUsuario, verificarContraseña, listarRoles} from "../controllers/usuarioController.js";
 import { validacionUser } from "../../validation/UsuariosValidator.js";
+import { validacionUserActualizar } from "../../validation/UsuariosValidatorUpdate.js";
 import { validarToken} from "../controllers/AutentificacionLogin.js";
 const rutaUsuario= Router()
 
@@ -11,7 +12,7 @@ rutaUsuario.post('/registrar', validacionUser, registrarUsuario)
 rutaUsuario.put('/contra/:id', verificarContraseña)
 rutaUsuario.delete('/eliminar/:id_usuario',  eliminarUsuario)
 rutaUsuario.put('/estado/:id_usuario',estadoUsuario)
-rutaUsuario.put('/actualizar/:id' ,actualizarUsuario)
+rutaUsuario.put('/actualizar/:id', validacionUserActualizar, actualizarUsuario)
 rutaUsuario.get('/consulta',ConsultaUsers)
 
 export default rutaUsuario
