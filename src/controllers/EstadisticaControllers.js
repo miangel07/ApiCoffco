62d8d37d2fa42b87e97ce1b0899467c7aed8718a
miangel07 <miguelosoriorojas063@gmail.com>
2024-10-14 23:54:55 -0500

    generando consulta de alquiler de laboratorio

--- src/controllers/EstadisticaControllers.js
+++ src/controllers/EstadisticaControllers.js
@@ -36,3 +36,33 @@ ORDER BY
         res.status(500).json({ message: "Error en la conexion" + error.message });
     }
 }
+
+export const EstadisticasAlquiler = async (req, res) => {
+    try {
+        let slq = `SELECT 
+                DATE_FORMAT(fecha, '%Y-%m') AS fecha, 
+                COUNT(*) AS cantidad_servicios
+            FROM 
+                servicios
+            WHERE 
+                fk_idTipoServicio = 4
+                AND fecha BETWEEN CURDATE() - INTERVAL 4 MONTH AND CURDATE() + INTERVAL 4 MONTH
+            GROUP BY 
+                fecha
+            ORDER BY 
+                fecha;
+
+
+`
+        const [resultados] = await conexion.query(slq);
+        if (resultados.length > 0) {
+            return res.status(200).json(resultados);
+        }
+
+        return res.status(404).json({ message: "No se encontraron datos" });
+
+    } catch (error) {
+        return res.status(500).json({ message: error });
+
+    }
+}
--- src/routes/EstadisticaRouters.js
+++ src/routes/EstadisticaRouters.js
@@ -1,9 +1,10 @@
-import { Estadistica } from "../controllers/EstadisticaControllers.js";
+import { Estadistica, EstadisticasAlquiler } from "../controllers/EstadisticaControllers.js";
 import { validarToken } from "../controllers/AutentificacionLogin.js";
 
 import Router from "express";
 const EstadisticaRouter = Router()
 
 EstadisticaRouter.get('/estadistica', validarToken, Estadistica)
+EstadisticaRouter.get('/alquiler', validarToken, EstadisticasAlquiler)
 
 export default EstadisticaRouter
